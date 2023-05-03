1503e433c2200c7d9cb229ec67b21788d480d904
Sheikhmsip <sheikhmsip@gmail.com>
2023-05-03 23:11:42 +0600

    added json data  on server

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
+const chefs = require('./data/chef.json');
 
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('chef-hunter is running')
 })
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
+
 
 app.listen(port, () => {
     console.log(`chef API ins running on : ${port}`)
