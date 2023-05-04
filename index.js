d82f8ee9359aad2469e7230d41f147d26f89ff57
Sheikhmsip <sheikhmsip@gmail.com>
2023-05-04 18:05:48 +0600

    update server site

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/chefs', (req, res) => {
     res.send(chefs);
 })
 
-app.get('/chefs/:id', (req, res) => {
+app.get('/:id', (req, res) => {
     const id = parseInt(req.params.id);
     console.log(id)
     const chefsCategory = chefs.filter(c => parseInt(c.id) === id);
